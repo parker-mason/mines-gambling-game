457fc5830cc75c0e51b8ab91d9b7d519549a91b2
Mason <s430385@s.cusd80.com>
2024-12-10 13:32:19 -0700

    add simple pygame window

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("hello world")
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((500, 500))
+
+running = True
+
+while running:
+  pygame.display.flip()
+
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      running = False
+
+pygame.quit()
