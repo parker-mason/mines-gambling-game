3efa6da48cd58f4a46d5df4088fdb8c4be3e25e7
Andrew Bogle <thachookie@gmail.com>
2024-12-10 21:21:26 -0700

    changes the color back and forth between red and purple

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ pygame.init()
 
 background_color = (192, 71, 252)
 
+color_value = 0
+
 screen = pygame.display.set_mode((500, 500))
 
 screen.fill(background_color)
 
 running = True
+
+
 
 while running:
   pygame.display.flip()
@@ -16,5 +20,13 @@ while running:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       running = False
+    elif event.type == pygame.MOUSEBUTTONDOWN:
+      if color_value == 0:
+        screen.fill((255,0,0))
+        color_value = color_value + 1
+      elif color_value == 1:
+        screen.fill(background_color)
+        color_value = color_value - 1
 
 pygame.quit()
+
