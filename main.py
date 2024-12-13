8a4744aef6c1e110f1481b08be39a220445a45f2
Parker Mason <parkermason123456@gmail.com>
2024-12-12 23:54:21 -0700

    can interact with gems, but is not specific to each gem yet

--- main.py
+++ main.py
@@ -3,69 +3,67 @@ import pygame #fuck you andrew, you are demoted to my 5th favorite black person,
 pygame.init()
 
 class Sprite(pygame.sprite.Sprite):
-  def __init__(self, image, x, y, func):
+  def __init__(self, image):
     super().__init__()
     self.image = pygame.image.load(image)
-    self.rect = self.image.get_rect()
-    self.rect.x = x
-    self.rect.y = y
-    self.callback = func
-
-  def update(self, events):
-    for event in events:
-      if event.type == pygame.MOUSEBUTTONUP:
-        if self.rect.collidepoint(event.pos):
-          self.callback()
-
-def process_click():
-  print("a gem was clicked")
 
 BACKGROUND_COLOR = (39, 48, 46)
+WINDOW_HEIGHT = 900
+WINDOW_LENGTH = 1200
+
+screen = pygame.display.set_mode((WINDOW_LENGTH, WINDOW_HEIGHT))
+
+gems = []
+
+gem = Sprite("Assets/Gem.png")
+
 GEM_WIDTH = 144
 GEM_HEIGHT = 144
 MAX_GEM_COUNT = 25
 MAX_GEMS_TO_PRINT = 5
-
-WINDOW_HEIGHT = 900
-WINDOW_LENGTH = 1200
 HORIZONTAL_PADDING = 80
 VERTICAL_PADDING = 30
-
 gem_counter = MAX_GEM_COUNT
-gems_to_print = MAX_GEMS_TO_PRINT
-can_print = True
 x = 0
 y = 0
 
-gem = Sprite("Assets/Gem.png", x, x, process_click)
+def process_click():
+  print("a gem was clicked")
+
+def get_clicked(events):
+  for event in events:
+    if event.type == pygame.MOUSEBUTTONUP:
+      for i in range(0, 25):
+        if gems[i].collidepoint(event.pos):
+          process_click()
 
 def load_sprite(sprite, x, y):
   screen.blit(sprite.image, (x, y))
-  
-screen = pygame.display.set_mode((WINDOW_LENGTH, WINDOW_HEIGHT))
+  gems.append(sprite.image.get_rect())
+  gems[-(gem_counter-25)].x = x
+  gems[-(gem_counter-25)].y = y
 
 screen.fill(BACKGROUND_COLOR)
 
 running = True
 
+displayed_gems = False
+
 while running:
 
-  if gems_to_print == MAX_GEMS_TO_PRINT:
-    x += HORIZONTAL_PADDING
-    y += VERTICAL_PADDING
-    gems_to_print -= 1
-    can_print = True
-  elif gems_to_print > 0:
-    x += GEM_WIDTH + HORIZONTAL_PADDING
-    gems_to_print -= 1
-  else:
-    x = 0
-    y += GEM_HEIGHT
-    gems_to_print = MAX_GEMS_TO_PRINT
-    can_print = False
-  if gem_counter > 0 and can_print:
-    load_sprite(gem, x, y)
-    gem_counter -= 1
+  if not displayed_gems:
+    for i in range(0, MAX_GEMS_TO_PRINT):
+      y += VERTICAL_PADDING
+      for j in range(0, MAX_GEMS_TO_PRINT):
+        if j == 0:
+          x += HORIZONTAL_PADDING
+        else:
+          x += HORIZONTAL_PADDING + GEM_WIDTH
+        load_sprite(gem, x, y)
+        gem_counter -= 1
+      y += GEM_HEIGHT
+      x = 0
+    displayed_gems = True
 
   keys = pygame.key.get_pressed()
   events = pygame.event.get()
@@ -78,9 +76,8 @@ while running:
         print("quit shortcut key pressed\nexiting program...")
         running = False
         
-  gem.update(events)
+  get_clicked(events)
 
   pygame.display.update()
 
 pygame.quit()
-
