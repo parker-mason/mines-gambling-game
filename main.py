08fdd466d5963693e5478924c933a041a75d5cb0
Parker Mason <parkermason123456@gmail.com>
2024-12-15 22:56:58 -0700

    reveal tiles after click, bug with gem 25

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame #fuck you andrew, you are demoted to my 5th favorite black person, you should give me sloppy slop in the bathroom to compensate for your errors, I will taste your insides. - Parker Ray Mason 
 
+import time
+
 import random
 
 pygame.init()
@@ -18,9 +20,11 @@ screen = pygame.display.set_mode((WINDOW_LENGTH, WINDOW_HEIGHT))
 randmine = random.randint(1,25)
 
 board = []
+clicked_tiles = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0 ,0 ,0 ,0 ,0 ,0]
 
 gem = Sprite("Assets/Gem.png")
 mine = Sprite("Assets/Mine.png")
+blank_tile = Sprite("Assets/Blank Tile.png")
 
 GEM_WIDTH = 144
 GEM_HEIGHT = 144
@@ -38,16 +42,21 @@ def get_clicked(events):
   for event in events:
     if event.type == pygame.MOUSEBUTTONUP:
       for i in range(0, 25):
-          if board[i].collidepoint(event.pos):
+          if board[i].collidepoint(event.pos) and clicked_tiles[i] != 1:
             if i+1 == randmine:
+              load_sprite(mine, board[i].x, board[i].y)
+              clicked_tiles[i] = 1
               print("you clicked a mine stupidface")
             else:
-              print(f"You clicked gem number {i+1}")
+              load_sprite(gem, board[i].x, board[i].y)
+              clicked_tiles[i] = 1
+              print(f"You clicked gem number {i+1} ({board[i].x}, {board[i].y})")
+            time.sleep(2)
 
 def load_sprite(sprite, x, y):
   screen.blit(sprite.image, (x, y))
-  board.append(sprite.image.get_rect()) #the get_rect function returns a rectangle the size of the image calling it
-  board[gem_counter-1].x = x        #could have also done gems.append((144, 144), (x, y))
+  board.append(sprite.image.get_rect())
+  board[gem_counter-1].x = x
   board[gem_counter-1].y = y
 
 screen.fill(BACKGROUND_COLOR)
@@ -67,12 +76,10 @@ while running:
         else:
           x += HORIZONTAL_PADDING + GEM_WIDTH
         gem_counter += 1
-        if randmine == gem_counter:
-          load_sprite(mine, x, y)
-        else:
-          load_sprite(gem, x, y)
+        load_sprite(blank_tile, x, y)
       y += GEM_HEIGHT
       x = 0
+    print(randmine)
     displayed_gems = True
 
   keys = pygame.key.get_pressed()
@@ -84,7 +91,7 @@ while running:
     if event.type == pygame.KEYDOWN:
       if event.key == pygame.K_RSHIFT:
         print("quit shortcut key pressed\nexiting program...")
-        print(randmine)
+        print(board)
         running = False
         
   get_clicked(events)
